62dc15a792027b05dad969c4f4b64a6d2d1e2376
Nicolaj Figaw <figaw@hotmail.com>
2016-08-25 14:39:53 +0200

    what

--- jake.sh
+++ jake.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Mathemagical!"
+echo "Mathemagica!"
 echo "jajaja"
+echo "did more stuff"
 echo "hmm"
 echo "I wasn't scared! I was singing! I was singing my scream song. Wooaah! Wooaaooh!"
 echo "I love Lady Rainicorn"
